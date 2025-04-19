3c061b48e06a19733fedbd2dd102884eb77335be
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2025-04-19 14:59:53 +0100

    Added functionality to home button

--- src/components/TitleBar/index.tsx
+++ src/components/TitleBar/index.tsx
@@ -1,8 +1,15 @@
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import "./index.css"
 
 function TitleBar() {
   const { pathname } = useLocation()
+  const navigate = useNavigate();
+
+
+  function navigateHome() {
+    const url = "/"
+    navigate(url)
+  }
 
   const title = "Apprendre la vérification avec Cameleer"
   const titleStyle = pathname == "/" ? "titleBar-title-container" :
@@ -16,7 +23,7 @@ function TitleBar() {
       <>{
         pathname == "/" ?
           <></> :
-          <div className="titleBar-item-container">Home</div>
+          <div className="titleBar-item-container" onClick={navigateHome}>Home</div>
       }</>
       <div className="titleBar-item-container">Related Work</div>
       <div className="titleBar-item-container">About Us</div>
