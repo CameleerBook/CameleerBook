62be8c160a285638b8a91fad96797c88ab20f06e
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2025-04-19 15:16:16 +0100

    Darker color in TOC to indicate which chapter user is in

--- src/components/TableOfContentsItem/index.tsx
+++ src/components/TableOfContentsItem/index.tsx
@@ -1,14 +1,28 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { Chapter } from "../../utils/types";
 import "./index.css"
 import { navigateToChapter } from "../../utils/utils";
 
 function TableOfContentsItem(props: { chapter: Chapter }) {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
   const chapter = props.chapter
+  const number = chapter.number
+
+  function getStyle(pathname: string, n: number) {
+    const aux = pathname.split("/")
+    const prefix = "toc-item"
+    if (aux[aux.length - 1] == n.toString()) {
+      return prefix + " toc-selected"
+    }
+
+    return prefix + " toc-notselected"
+  }
+
+  const style = getStyle(pathname, number)
 
   return (
-    <div className="toc-item" onClick={() => navigateToChapter(chapter, navigate)}>
+    <div className={style} onClick={() => navigateToChapter(chapter, navigate)}>
       <p>{chapter.name}</p>
     </div>
   )
