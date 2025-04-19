0a6f95254efc89b626bc3a2c20c76c9bdeb94fd0
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2025-04-19 14:57:10 +0100

    Added dinamic home button to title bar

--- src/components/TitleBar/index.tsx
+++ src/components/TitleBar/index.tsx
@@ -1,9 +1,23 @@
+import { useLocation } from "react-router";
 import "./index.css"
 
 function TitleBar() {
+  const { pathname } = useLocation()
+
+  const title = "Apprendre la vérification avec Cameleer"
+  const titleStyle = pathname == "/" ? "titleBar-title-container" :
+    "titleBar-smallTitle-container"
+
   return (
     <div className="titleBar-container">
-      <div className="titleBar-title">Apprendre la vérification avec Cameleer</div>
+      <div className={titleStyle}>
+        <p className="titleBar-title">{title}</p>
+      </div>
+      <>{
+        pathname == "/" ?
+          <></> :
+          <div className="titleBar-item-container">Home</div>
+      }</>
       <div className="titleBar-item-container">Related Work</div>
       <div className="titleBar-item-container">About Us</div>
     </div>
