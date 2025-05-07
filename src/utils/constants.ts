badcc80024f7250b93d396427d0f4ddbb8c7ac79
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2025-05-07 22:19:18 +0100

    Updated home text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,29 +5,28 @@ const INTRO_MSG_CONTEXT: IntroMessage = {
   text: `Apprendre la vérification avec Cameleer is a textbook for learning 
   Formal Verification of software through the use of automated deductive tools, 
   such as Cameleer, Why3 and SMT solvers. Formal Verification is a field of 
-  Computer Science concerned with the proving the soundness of the reasoning and
-  implementation of a piece of software, through the use of logical statements 
-  and mathematical properties.`
+  Computer Science concerned with proving the soundness of the reasoning and
+  implementation of a piece of software with logical statements and mathematical 
+  properties.`
 }
 
 const INTRO_MSG_MOTIVATION: IntroMessage = {
   title: "Motivation",
-  text: `In various contexts software failure can lead to undesired and serious 
-  consequences, for instance endangering human lives. Verifying of software 
-  correctness through unit testing is unreliable and insufficient, as any 
-  industrial-grade software has an unimaginable number of possible states. 
-  Instead, we must rely on Formal Verification, and although not an easy 
-  endeavour, this can be facilitated to an extent with automated deductive 
-  tools.`
+  text: `In various contexts, software failure can lead to undesired and serious 
+  consequences, for instance endangering human lives. Unit testing by itself is 
+  unreliable and insufficient, as any industrial-grade software has an 
+  unimaginable number of possible states. Instead, we must rely on Formal 
+  Verification, and although not an easy endeavour, this can be facilitated to 
+  an extent with automated deductive tools.`
 }
 
 const INTRO_MSG_OVERVIEW: IntroMessage = {
   title: "Overview",
   text: `"Apprendre la vérification avec Cameleer" uses classical algorithms and 
-  data structures as a vehicle to teach about Formal Verification. By using 
-  well-known algorithms, we believe most readers will be vaguely familiarized 
-  with the problem or, at the very least, can be easily searched on other 
-  sources of information. In this book we use Cameleer, Why3 and a few SMT 
+  data structures as a vehicle to teach about Formal Verification. We believe 
+  most readers will be vaguely familiarized with the set of well-known algorithms 
+  present in the book, or at the very least, may easily find information on other 
+  sources and learning materials. In this book we use Cameleer, Why3 and a few SMT 
   solvers as our primary tools, as we want to provide the next generation of
   Verification Engineer with both practical and theoretical knowledge.`
 }
