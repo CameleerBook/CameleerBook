932bfe48b405b4431b522b6110589ee7c0cde15a
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2025-06-28 22:05:29 +0100

    Changed base url

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import HomePage from './pages/HomePage';
 import ChapterPage from './pages/ChapterPage';
 import RelatedPage from './pages/RelatedPage';
 import GalleryPage from './pages/GalleryPage';
-import { ABOUT, GALLERY, HOME, RELATED_WORK } from './utils/navigation';
+import { ABOUT, CHAPTER, GALLERY, HOME, RELATED_WORK } from './utils/navigation';
 import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <Routes>
       <Route path={HOME} element={<HomePage />} />
-      <Route path='/chapter/:id' element={<ChapterPage />} />
+      <Route path={CHAPTER+':id'} element={<ChapterPage />} />
       <Route path={GALLERY} element={<GalleryPage />} />
       <Route path={RELATED_WORK} element={<RelatedPage />} />
       <Route path={ABOUT} element={<AboutPage />} />
