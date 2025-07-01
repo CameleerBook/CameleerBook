ccb5ab9f60f435ac59e4a6e06d75091e3298f641
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2025-07-01 21:43:54 +0100

    Homepage: more reactive

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -7,16 +7,16 @@ import "./index.css"
 function HomeBody() {
   return (
     <div className="homePage-container">
-      <div className="homePage-element">
+      <div className="homePage-left">
         <div className="homePage-intro">
           <IntroParagraph msg={INTRO_MSG_VERIFICATION} />
           <IntroParagraph msg={INTRO_MSG_AUTOMATED} />
         </div>
       </div>
-      <div className="homePage-element">
-        <CoverItem width={412.5} height={637.5} />
+      <div className="homePage-image">
+        <CoverItem width={330} height={510} />
       </div>
-      <div className="homePage-element">
+      <div className="homePage-right">
         <div className="homePage-intro">
           <IntroParagraph msg={INTRO_MSG_ALGORITHMS} />
           <IntroParagraph msg={INTRO_MSG_CAMELEER} />
@@ -29,6 +29,7 @@ function HomeBody() {
 /*
   Dimensooes com butoes
   <CoverItem width={330} height={510} />
+  <CoverItem width={412.5} height={637.5} />
 
 */
 
